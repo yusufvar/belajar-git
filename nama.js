a589aa4d2e119ee847848509993ec7f52699a3de
yusufvar <amiry3398@gmail.com>
2023-04-05 13:47:14 +0700

    saya menambahkan variable kategori di nama.js

--- nama.js
+++ nama.js
@@ -1,5 +1,8 @@
 var nama = "yusuf";
 var universitas = "universitas muhammadiyah gresik"
 var usia = 22;
+var kategori = usia < 23 ? ('masih muda') : ('sudah tua')
 
-prompt ('nama saya '+nama+' sekarang saya kuliah di'+universitas+', dan usia saya tahun ini '+usia+' tahun.');
+
+
+prompt ('nama saya '+nama+' sekarang saya kuliah di '+universitas+', dan usia saya tahun ini '+usia+' tahun. Di usia saya sekarang saya masih tergolong '+kategori);
