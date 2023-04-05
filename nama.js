683c4c15b5742c6aca7c9932a3af2b61fe2be319
yusufvar <amiry3398@gmail.com>
2023-04-05 15:16:58 +0700

    saya menambahkan variabel kabupaten di nama.js

--- nama.js
+++ nama.js
@@ -5,4 +5,4 @@ var golongan = usia < 23 ? ('masih muda') : ('sudah tua')
 var kecamatan = "benjeng"
 var kabupaten = "gresik"
 
-prompt ('nama saya '+nama+' sekarang saya kuliah di '+universitas+', dan usia saya tahun ini '+usia+' tahun. Di usia saya sekarang saya masih tergolong '+golongan+'. Saya bertempat tinggal di kecamatan'+kecamatan);
+prompt ('nama saya '+nama+' sekarang saya kuliah di '+universitas+', dan usia saya tahun ini '+usia+' tahun. Di usia saya sekarang saya masih tergolong '+golongan+'. Saya bertempat tinggal di kecamatan'+kecamatan+', kabupaten '+kabupaten);
