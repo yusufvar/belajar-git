ee9b1d5909db1f4b4eeef1a83ccaaec27929de02
yusufvar <amiry3398@gmail.com>
2023-04-13 16:34:50 +0700

    membuat calculator

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-var nama = "yusuf"
-var universitas = "universitas muhammadiyah gresik"
-var usia = 22;
-var golongan = usia < 23 ? ('masih muda') : ('sudah tua')
-var kecamatan = "benjeng"
-var kabupaten = "gresik"
-var jawaban = ['sudah', 'belum'];
-var jawab = jawaban[Math.floor(Math.random() * jawaban.length)];
-function javascribt() {
-    console.log("JavaScript adalah suatu bahasa pemrograman tingkat tinggi dan dinamis. JavaScript populer di internet dan dapat bekerja di sebagian besar penjelajah web populer seperti Google Chrome, Internet Explorer, Mozilla Firefox, Netscape dan Opera. Kode JavaScript dapat disisipkan dalam halaman web menggunakan tag script.")
-}
-
-const jumlah = (a, b) => (a+b)
-
-
-
-prompt ('nama saya '+nama+' sekarang saya kuliah di '+universitas+', dan usia saya tahun ini '+usia+' tahun. Di usia saya sekarang saya masih tergolong '+golongan+'. Saya bertempat tinggal di kecamatan'+kecamatan+', kabupaten '+kabupaten+'. Di usia saya sekarang saya '+jawab+' mempunyai kekasih, Terima kasih.');
-
-alert (javascribt);
-
-alert (jumlah(1, 2))
-
-// for (var i = 1; i = 5; i++){
-//     prompt ("ini hasil dari for "+i)
+// var nama = "yusuf"
+// var universitas = "universitas muhammadiyah gresik"
+// var usia = 22;
+// var golongan = usia < 23 ? ('masih muda') : ('sudah tua')
+// var kecamatan = "benjeng"
+// var kabupaten = "gresik"
+// var jawaban = ['sudah', 'belum'];
+// var jawab = jawaban[Math.floor(Math.random() * jawaban.length)];
+// function javascribt() {
+//     console.log("JavaScript adalah suatu bahasa pemrograman tingkat tinggi dan dinamis. JavaScript populer di internet dan dapat bekerja di sebagian besar penjelajah web populer seperti Google Chrome, Internet Explorer, Mozilla Firefox, Netscape dan Opera. Kode JavaScript dapat disisipkan dalam halaman web menggunakan tag script.")
 // }
 
-var berapa = 5
-for (var i = berapa; i >= 1; i--){
-    var bintang = " "
-    for (var j = 1; j <= i; j++){
-        bintang += "*"
-    }
-    console.log (bintang)
-}
+// const jumlah = (a, b) => (a+b)
 
-for (var i = 1; i<=berapa; i++){
-    var bintang = " "
-    for (var j = 1; j <= i; j++){
-        bintang += "*"
-    }
-    console.log (bintang)
+
+
+// prompt ('nama saya '+nama+' sekarang saya kuliah di '+universitas+', dan usia saya tahun ini '+usia+' tahun. Di usia saya sekarang saya masih tergolong '+golongan+'. Saya bertempat tinggal di kecamatan'+kecamatan+', kabupaten '+kabupaten+'. Di usia saya sekarang saya '+jawab+' mempunyai kekasih, Terima kasih.');
+
+// alert (javascribt);
+
+// alert (jumlah(1, 2))
+
+// // for (var i = 1; i = 5; i++){
+// //     prompt ("ini hasil dari for "+i)
+// // }
+
+// var berapa = 5
+// for (var i = berapa; i >= 1; i--){
+//     var bintang = " "
+//     for (var j = 1; j <= i; j++){
+//         bintang += "*"
+//     }
+//     console.log (bintang)
+// }
+
+// for (var i = 1; i<=berapa; i++){
+//     var bintang = " "
+//     for (var j = 1; j <= i; j++){
+//         bintang += "*"
+//     }
+//     console.log (bintang)
+// }
+
+let num1 = parseFloat(prompt("Masukkan bilangan pertama : "))
+let num2 = parseFloat(prompt("Masukkan biilangan kedua : "))
+
+if (angka(num1) || angka(num2)){
+    alert ("Angka yang di masukkan salah!!!")
+} else {
+    alert('Hasil penjumlahan : ${num1 + num2}, Hasil penjumlahan : ${num1 - num2}')
 }
